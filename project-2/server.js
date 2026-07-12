27daf9edb873f090cdd84453448066c1975bd123
Ademozi <ademoviadem@gmail.com>
2026-07-12 09:56:16 +0100

    get user by id route

--- project-2/server.js
+++ project-2/server.js
@@ -11,9 +11,33 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 
+app.post("/users", (req, res) => {
+    const newUser = new User();
+
+    const userEmail = req.body.userEmail;
+    const userAge = req.body.userAge;
+    const userActivity = req.body
+});
+
 app.get('/users', async (req, res) => {
     const users = await User.find();
     res.json(users);
+});
+
+app.get('/users/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const user = await User.findById(id);
+        if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+        return;
+    } catch (error) {
+        console.log("error while reading user of id ", id);
+        return res.json(error);
+    }
 });
 
 
